7e964fd850c0cbfea226631ead2efd308ba1d839
Akumas <70409393+Akvmas@users.noreply.github.com>
2023-12-13 12:05:08 +0100

    Update db.php

--- src/db.php
+++ src/db.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
 $db   = 'NexaStream';
-$user = 'root';
-$pass = 'FuRyAx2004';
+$user = '';
+$pass = '';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
